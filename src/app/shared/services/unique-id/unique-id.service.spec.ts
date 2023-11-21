cd2a2517f99af74277877d8407967b773e6016a8
JehSA <jef.almeida@gmail.com>
2023-11-21 10:07:00 -0300

    First commit.

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,29 @@
+import { UniqueIdService } from "./unique-id.service";
+
+describe(UniqueIdService.name, () => {
+
+    let service: UniqueIdService = null;
+    beforeEach(() => {
+        service = new UniqueIdService();
+    });
+    
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called eith prefix`, () => {        
+        const id = service.generateUniqueIdWithPrefix('app');
+        expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {        
+        const ids = new Set();
+        for (let i = 0; i < 50; i++) {
+            ids.add(service.generateUniqueIdWithPrefix('app'));
+        }
+        expect(ids.size).toBe(50);
+    });
+
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generated Ids when called`, () => {        
+        service.generateUniqueIdWithPrefix('app');
+        service.generateUniqueIdWithPrefix('app');
+        expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+    });
+
+});
---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { v4 as uuidv4 } from "uuid";
+
+@Injectable()
+export class UniqueIdService {
+
+    private numberOfGeneratedIds = 0;
+
+    public generateUniqueIdWithPrefix(prefix: string): string {
+        if (!prefix) {
+            throw Error('Prefix can not be empty');
+        }
+        const uniqueId = this.generateUniqueId();
+        this.numberOfGeneratedIds++;
+        return `${prefix}-${uniqueId}`;
+    }
+
+    public getNumberOfGeneratedUniqueIds(): number {
+        return this.numberOfGeneratedIds;
+    }
+
+    public generateUniqueId(): string {
+        return uuidv4();
+    }
+}
