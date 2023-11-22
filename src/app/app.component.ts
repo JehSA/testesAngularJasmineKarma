d5f496db6b0b1995f3f5cda33836b1756660dfa1
JehSA <jef.almeida@gmail.com>
2023-11-22 10:57:55 -0300

    componente funcionando.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'Angular testing';
+
+  public likes = 0;
+
+  public like(): void {
+    this.likes++;
+  }
+
 }
